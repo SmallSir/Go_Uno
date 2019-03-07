3765a1ec54d7007b8b70ced7a30ee5db3f1b728a
WhaleFall <280690956@qq.com>
2019-03-07 17:44:29 +0800

    增加了前端所需要发送给后端的信息格式

---
+++ Uno/controllers/messages.go
@@ -0,0 +1,21 @@
+package controllers
+
+//创建房间or加入房间
+type RoomMsg struct {
+	//房间名称
+	RoomName     string `json:"name"`
+	//房间密码
+	RoomPassWord string `json:"password"`
+}
+
+//出牌状态
+type CardStateMsg struct{
+	//出牌的颜色
+	Color string `json:"color"`
+	//出牌的号码
+	Number string `json:"number"`
+	//出牌的数量
+	Numbers string `json:"numbers"`
+	//出牌状态
+	State string `json:"state"` 
+}
