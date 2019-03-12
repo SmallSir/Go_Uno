e0ef2790afba10c39b57251965ba1887f02abd3a
WhaleFall <280690956@qq.com>
2019-03-12 10:58:16 +0800

    实现了手牌快拍的效果

--- Uno/controllers/usercard.go
+++ Uno/controllers/usercard.go
@@ -1,6 +1,5 @@
 package controllers
 
-
 type UserCard struct {
 	cards  []Card
 	number int
@@ -8,58 +7,75 @@ type UserCard struct {
 
 //手牌排序
 func (user *UserCard) Sort() {
-	user.QuickSort(0,user.number - 1)
+	user.QuickSort(0, user.number-1)
 }
 
 //快排
-func (user *UserCard) QuickSort(l,r int){
-	if l >= r{
-		return 
+func (user *UserCard) QuickSort(l, r int) {
+	if l >= r {
+		return
 	}
-	i,j,key := l,r,user.cards[l]
-	for i<j{
-		for i < j && user.Compare(user.cards[j],key) {
-			j--;
+	mid := user.cards[(l+r)/2]
+	x := l
+	y := r
+	for x < y {
+		for user.Compare(user.cards[x], mid) {
+			x++
 		}
-		if i < j{
-			user.cards[i] = user.cards[j]
-			i++
+		for user.Compare(mid, user.cards[y]) {
+			y--
 		}
-		
+		if x <= y{
+			tmp := user.cards[x]
+			user.cards[x] = user.cards[y]
+			user.cards[y] = tmp
+			x++
+			y--
+		}
 	}
+	user.QuickSort(l,y)
+	user.QuickSort(x,r)
 }
 
 //两张牌比大小
-func (user *UserCard) Compare(x Card,y Card) bool{
-	if(x.color == y.color){
-		if x.number == "-1"{
-			if y.number == "-1"{
+func (user *UserCard) Compare(x Card, y Card) bool {
+	//两张牌颜色相同
+	if x.color == y.color {
+		//如果x为功能牌
+		if x.number == "-1" {
+			//如果y为功能牌
+			if y.number == "-1" {
 				return x.state < y.state
-			}else{
+				//如果y不是功能牌
+			} else {
 				return false
 			}
-		}else{
-			if y.number == "-1"{
+			//如果x不是功能牌
+		} else {
+			//如果y是功能牌
+			if y.number == "-1" {
 				return true
-			} else{
+				//如果y不是功能牌可以直接比较
+			} else {
 				return x.number < y.number
 			}
 		}
+		//两张牌颜色不同
 	} else {
 		return x.color < y.color
 	}
 }
 
 //二分查找
-func (user *UserCard) BinaryInsert(new_card Card) int{
+func (user *UserCard) BinaryInsert(new_card Card) int {
 	l := 0
 	r := user.number - 1
 	for l < r-1 {
 		mid := (l + r) / 2
-		check := user.Compare(user.cards[mid],new_card)
+		check := user.Compare(user.cards[mid], new_card)
 		if check {
 			l = mid
-		} else{
+		} else {
 			r = mid
 		}
 	}
@@ -68,9 +84,9 @@ func (user *UserCard) BinaryInsert(new_card Card) int{
 
 //添加新牌
 func (user *UserCard) Insert_Card(newcards []Card) {
-	for _,new_card := range newcards{
+	for _, new_card := range newcards {
 		r := user.BinaryInsert(new_card)
-		new_cards := append(user.cards[:r-1],new_card)
+		new_cards := append(user.cards[:r-1], new_card)
 		new_cards = append(user.cards[r:])
 		user.cards = new_cards
 		user.number += 1
@@ -78,12 +94,11 @@ func (user *UserCard) Insert_Card(newcards []Card) {
 }
 
 //移除牌
-func (user *UserCard) Remove_Card(oldcards []Card){
-	for _,old_card := range oldcards{
+func (user *UserCard) Remove_Card(oldcards []Card) {
+	for _, old_card := range oldcards {
 		r := user.BinaryInsert(old_card)
-		new_cards := append(user.cards[:r-1],user.cards[r+1:]...)
+		new_cards := append(user.cards[:r-1], user.cards[r+1:]...)
 		user.cards = new_cards
 		user.number += 1
 	}
 }
-
