7241f9d96f925f98cf96e8d4ead5e481133e9ed0
WhaleFall <280690956@qq.com>
2019-03-14 17:07:06 +0800

    修改了之前range的bug

--- Uno/controllers/room.go
+++ Uno/controllers/room.go
@@ -11,7 +11,7 @@ type PlayerRoom struct {
 	//已经准备人数
 	ready_number int
 	//玩家信息
-	players []Player
+	players []*Player
 	//房间信息
 	player_room Room
 	//目前已有的人数
@@ -24,7 +24,7 @@ type PlayerRoom struct {
 	dirction int
 	//当前的号码
 	latest_number string
-	//当前状态 
+	//当前状态
 	latest_state string
 	//玩家编号(东北西南)
 	playerno []int
@@ -33,11 +33,11 @@ type PlayerRoom struct {
 }
 
 //创建房间
-func (rm *PlayerRoom) Room(room Room, number int, player Player) *PlayerRoom {
+func (rm *PlayerRoom) Room(room Room, number int, player_id int) *PlayerRoom {
 	newroom := PlayerRoom{players_number: number, players: make([]Player, number),
-		player_room: room, ready_number: 0,dirction:0,
+		player_room: room, ready_number: 0, dirction: 0,
 		stay_number: 1, playerno: make([]int, 4, 4), nextplayer: 0}
-	_, err := newroom.AddPlayer(player)
+	_, err := newroom.AddPlayer(player_id)
 	if err != nil {
 		log.Println(err)
 	}
@@ -45,12 +45,17 @@ func (rm *PlayerRoom) Room(room Room, number int, player Player) *PlayerRoom {
 }
 
 //添加玩家
-func (rm *PlayerRoom) AddPlayer(player Player) (bool, error) {
+func (rm *PlayerRoom) AddPlayer(player_id int) (bool, error) {
 	if rm.stay_number == rm.players_number {
 		return false, errors.New("人数已满不能，请稍后再试")
 	}
 	rm.stay_number++
-	player.room_name = rm.player_room.room_name
+	for i, p := range rm.players {
+		if p.player_id == player_id {
+			rm.players[i].room_name = rm.player_room.room_name
+			break
+		}
+	}
 	for i, p := range rm.playerno {
 		if p == 0 {
 			rm.playerno[i] = player.player_id
@@ -85,9 +90,15 @@ func (rm *PlayerRoom) RemovePlayer(player Player) (bool, error) {
 }
 
 //玩家准备
-func (rm *PlayerRoom) ReadyPlayer() error {
+func (rm *PlayerRoom) ReadyPlayer(player_id int) error {
 	if rm.ready_number == rm.players_number {
 		return errors.New("人数已经满了不得再准备")
+	}
+	for i, p := range rm.players {
+		if p.player_id == player_id {
+			rm.players[i].state = 1
+			break
+		}
 	}
 	rm.ready_number++
 	if rm.ready_number == rm.players_number {
@@ -97,9 +108,15 @@ func (rm *PlayerRoom) ReadyPlayer() error {
 }
 
 //玩家取消准备
-func (rm *PlayerRoom) UnreadyPlayer() error {
+func (rm *PlayerRoom) UnreadyPlayer(player_id int) error {
 	if rm.ready_number == 0 {
 		return errors.New("没有任何人准备，不存在准备状态")
+	}
+	for i, p := range rm.players {
+		if p.player_id == player_id {
+			rm.players[i].state = 0
+			break
+		}
 	}
 	rm.ready_number--
 	return nil
@@ -112,22 +129,22 @@ func (rm *PlayerRoom) PlayGame() {
 	rm.latest_state = "-1"
 	//rm.latest_id = -1
 	rm.room_cards.Start()
-	for _, p := range rm.players {
-		p.player_cards.cards = append(rm.room_cards.AddCards(7)[:])
-		p.player_cards.number = 7
-		p.player_cards.Sort()
+	for i, p := range rm.players {
+		rm.players[i].player_cards.cards = append(rm.room_cards.AddCards(7)[:])
+		rm.players[i].player_cards.number = 7
+		rm.players[i].player_cards.Sort()
 	}
 }
 
 //获得出牌信息
 func (rm *PlayerRoom) RemoveCard(p_id int, rc Card) (bool, error) {
-	if  rm.latest_state != rc.state && rm.latest_state != "-1" &&rm.latest_color != "null" && rm.latest_color != rc.color && rm.latest_number != rc.number && rc.state != "wildraw" && rc.state != "wild" {
+	if rm.latest_state != rc.state && rm.latest_state != "-1" && rm.latest_color != "null" && rm.latest_color != rc.color && rm.latest_number != rc.number && rc.state != "wildraw" && rc.state != "wild" {
 		return false, errors.New("出的牌不对")
 	}
-	for _, p := range rm.players {
+	for i, p := range rm.players {
 		if p.player_id == p_id {
 			//改变玩家手牌信息
-			p.player_cards.Remove_Card(rc)
+			rm.players[i].player_cards.Remove_Card(rc)
 			//改变这把的牌堆信息
 			rm.room_cards.OutCards(rc)
 			//修改目前信息
@@ -138,20 +155,20 @@ func (rm *PlayerRoom) RemoveCard(p_id int, rc Card) (bool, error) {
 			break
 		}
 	}
-	if rm.latest_state == "reverse"{
+	if rm.latest_state == "reverse" {
 		rm.dirction = 1 - rm.dirction
 	}
-	if rm.dirction == 0{
-		if rm.latest_state == "skip"{
+	if rm.dirction == 0 {
+		if rm.latest_state == "skip" {
 			rm.nextplayer = (rm.nextplayer + 2) % 4
 		} else {
 			rm.nextplayer = (rm.nextplayer + 1) % 4
 		}
-	} else{
-		if rm.latest_state == "skip"{
+	} else {
+		if rm.latest_state == "skip" {
 			rm.nextplayer = (rm.nextplayer + 2) % 4
-		} else{
-			rm.nextplayer = (rm.nextplayer + 3 ) % 4
+		} else {
+			rm.nextplayer = (rm.nextplayer + 3) % 4
 		}
 	}
 	return true, nil
@@ -164,9 +181,9 @@ func (rm *PlayerRoom) SelectColor(color string) {
 
 //获得摸牌信息
 func (rm *PlayerRoom) GetCard(p_id int, rn int) {
-	for _, p := range rm.players {
+	for i, p := range rm.players {
 		if p.player_id == p_id {
-			p.player_cards.Insert_Card(rm.room_cards.AddCards(rn))
+			rm.players[i].player_cards.Insert_Card(rm.room_cards.AddCards(rn))
 		}
 	}
 }
