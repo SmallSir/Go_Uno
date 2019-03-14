5f525ff2294ec3d8167e8023fb18c4910226c42c
WhaleFall <280690956@qq.com>
2019-03-14 17:32:29 +0800

    添加了玩家摸牌，出牌，创建房间，加入房间等功能

--- Uno/controllers/gameserver.go
+++ Uno/controllers/gameserver.go
@@ -9,14 +9,33 @@ type GameController struct {
 var roomlist *roomtable
 
 //初始化房间表
-func init(){
+func init() {
 	RoomList = newRoomTable()
 }
 
-//创建客户端
+//创建房间
+func (game *GameController) Register() {
+
+}
+
+//加入房间
+func (game *GameController) Join() {
+
+}
 
 //玩家出牌
+func (game *GameController) RemoveCards() {
+
+}
 
 //玩家摸牌
+func (game *GameController) AddCards() {
+
+}
 
 //玩家选颜色
+func (game *GameController) SelectColor() {
+	color := game.GetString("color")
+	room_id := game.GetSession("room_id")
+	flag := roomlist.CheckRoom(room_id)
+}
