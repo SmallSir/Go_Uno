7b245ed608c507622f19176db9552e4484957542
WhaleFall <280690956@qq.com>
2019-03-14 15:35:56 +0800

    增加了玩家websocket的通信信息

--- Uno/controllers/player.go
+++ Uno/controllers/player.go
@@ -1,5 +1,9 @@
 package controllers
 
+import (
+	"github.com/gorilla/websocket"
+)
+
 type Player struct {
 	//是否准备
 	state bool
@@ -11,6 +15,8 @@ type Player struct {
 	player_cards UserCard
 	//所在房间
 	room_name string
+	//WebSocket连接
+	rwc *websocket.Conn
 }
 /*
 //玩家选择准备or取消准备
