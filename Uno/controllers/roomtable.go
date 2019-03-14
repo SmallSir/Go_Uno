2948a00784dbe08cee662e686a430e92dca7a089
WhaleFall <280690956@qq.com>
2019-03-14 17:07:20 +0800

    修改了bug

--- Uno/controllers/roomtable.go
+++ Uno/controllers/roomtable.go
@@ -4,11 +4,32 @@ import "sync"
 
 type roomtable struct {
 	//锁
-	locak sync.Mutex
+	lock sync.Mutex
 	//房间表
 	rooms map[string]*PlayerRoom
 }
 
+//初始化房间表
 func newRoomTable() *roomtable {
 	return &roomtable{rooms: make(map[string]*PlayerRoom)}
+}
+
+//检测房间号是否存在
+func (rt *roomtable) CheckRoom(roomname string) bool{
+	rt.lock.Lock()
+	defer rt.lock.Unlock()
+
+	r := rt.rooms[roomname]
+	if r == nil{
+		return false
+	}
+	return true
+}
+
+//创建房间
+func (rt *roomtable) CreateRoom(roomname string roompassword,rwc{
+	rt.lock.Lock()
+	defer rt.lock.Unlock()
+
+
 }
