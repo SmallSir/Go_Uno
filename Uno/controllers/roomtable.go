3be1ac8a56a1fc66a0edb3a922c247fec6143c64
WhaleFall <280690956@qq.com>
2019-03-18 14:17:55 +0800

    实现删除房间功能

--- Uno/controllers/roomtable.go
+++ Uno/controllers/roomtable.go
@@ -1,7 +1,7 @@
 package controllers
 
 import (
-	"github.com/gorilla/websocket"
+	"log"
 	"sync"
 )
 
@@ -18,23 +18,31 @@ func newRoomTable() *roomtable {
 }
 
 //检测房间号是否存在
-func (rt *roomtable) CheckRoom(roomname string,roompassword string) bool{
+func (rt *roomtable) CheckRoom(roomname string, roompassword string) bool {
 	rt.lock.Lock()
 	defer rt.lock.Unlock()
-
 	r := rt.rooms[roomname]
-	if r.player_room.room_name == roomname && r.player_room.room_password == roompassword{
+	if r.player_room.room_name == roomname && r.player_room.room_password == roompassword {
 		return true
-	} else{
+	} else {
 		return false
 	}
 }
 
 //创建房间
-func (rt *roomtable) CreateRoom(roomname string,roompassword string) {
+func (rt *roomtable) CreateRoom(roomname string, roompassword string) {
 	rt.lock.Lock()
 	defer rt.lock.Unlock()
-	roommsg := Room{room_name:roomname,room_password:roompassword}
+	roommsg := Room{room_name: roomname, room_password: roompassword}
 	//player := NewPlayer(ws,playerid,playername,roomname)
-	rt.rooms[roomname] = NewRoom(roommsg,4)
+	rt.rooms[roomname] = NewRoom(roommsg, 4)
+	log.Printf("已经创建了 %s 房间",roomname)
+}
+
+//删除房间
+func (rt *roomtable) RemoveRoom(roomname string){
+	rt.lock.Lock()
+	defer rt.lock.Unlock()
+	delete(rt.rooms,roomname)
+	log.Printf("已经将 %s 房间删除",roomname)
 }
