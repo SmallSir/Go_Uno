90b6906da5ae98ac3dfee8c6a39396b5e0adf7a2
WhaleFall <280690956@qq.com>
2019-03-20 14:20:14 +0800

    增加了对房间状态的返回数据格式，以及修改了对房间操作的接收数据格式

--- Uno/controllers/messages.go
+++ Uno/controllers/messages.go
@@ -3,26 +3,34 @@ package controllers
 //创建房间or加入房间
 type RoomMsg struct {
 	//房间名称
-	RoomName     string `json:"name"`
+	RoomName string `json:"name"`
 	//房间密码
 	RoomPassWord string `json:"password"`
 }
 
+//房间状态返回值
+type ReRoomMsg struct {
+	//返回的url(创建房间使用)
+	Url string `json:"url"`
+	//返回信息
+	Msg string `json:"message"`
+	//异常状态(true为正常,false为异常)
+	Ok bool `json:"state"`
+}
+
 //出牌状态
-type CardStateMsg struct{
+type CardStateMsg struct {
 	//出牌的颜色
 	Color string `json:"color"`
 	//出牌的号码
 	Number string `json:"number"`
-	//出牌的数量
-	//Numbers string `json:"numbers"`
 	//出牌状态
 	State string `json:"state"`
 	//玩家行为
-	behavior string `json:"behavior"` 
+	Behavior string `json:"behavior"`
 }
 
-type Card struct{
+type Card struct {
 	//卡牌颜色
 	color string
 	//卡牌号码
@@ -32,7 +40,7 @@ type Card struct{
 }
 
 //房间信息
-type Room struct{
-	room_name string
+type Room struct {
+	room_name     string
 	room_password string
 }
