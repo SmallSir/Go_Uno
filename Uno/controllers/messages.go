cfb9c0c04e8bbaa63a98710e22e6d790c7902457
WhaleFall <280690956@qq.com>
2019-03-22 17:22:01 +0800

    修改了数据格式

--- Uno/controllers/messages.go
+++ Uno/controllers/messages.go
@@ -18,7 +18,7 @@ type CardStateMsg struct {
 	Color    string `json:"color"`     //出牌的颜色
 	Number   string `json:"number"`    //出牌的号码
 	State    string `json:"state"`     //出牌状态
-	Behavior int `json:"behavior"`  //玩家行为(1表示摸牌,0表示出牌)
+	Behavior int    `json:"behavior"`  //玩家行为(1表示摸牌,0表示出牌)
 	PlayerId int    `json:"playerid"`  //玩家id
 	CN       int    `json:"getnumber"` //玩家摸牌个数
 }
@@ -31,6 +31,7 @@ type ReCardMsg struct {
 	CardsNumbers int    `json:"cardsnumber"` //剩余牌的数量
 	Select       bool   `json:"select"`      //是否选色
 	C            []Card `json:"cards"`       //玩家现在手里的牌
+	Uno          bool   `json:"uno"`         //是否需要喊UNO
 }
 
 //出牌返回数据给其他玩家
@@ -39,9 +40,10 @@ type ReOtherCardMsg struct {
 	Number      string `json:"number"`      //出牌的号码
 	State       string `json:"state"`       //出牌状态
 	CardsNumber int    `json:"cardsnumber"` //出牌玩家剩余牌数
-	Check       bool   `json:"check"`       //是否该玩家出牌
-	Uno         int    `json:"uno"`         //0表示正常,1表示这个人可以喊Uno,-1表示玩家被禁止,2表示玩家+2,4表示玩家+4
+	Check       bool   `json:"check"`       //是否轮到该玩家
 	Direction   int    `json:"direction"`   //方向
+	Uno         bool   `json:"uno"`         //上一个玩家是否喊了UNO
+	Behavior    int    `json:"behavior"`    //轮到该玩家后允许的行为
 }
 type Card struct {
 	color  string //卡牌颜色
