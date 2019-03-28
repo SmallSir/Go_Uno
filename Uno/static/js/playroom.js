84aea668f2dfe1879c0da1fc7d3b0f24ba990f0a
WhaleFall <280690956@qq.com>
2019-03-28 17:02:47 +0800

    把之前的图片预处理删除，同时完成了牌堆，出牌堆，按钮，展示效果，顺时针等图标的位置

--- Uno/static/js/playroom.js
+++ Uno/static/js/playroom.js
@@ -6,82 +6,54 @@ var Application = PIXI.Application,
     container = PIXI.Container;
     graphics = PIXI.Graphics;
 
-//所有的牌+背面
-//蓝色
-/*var b0 = Sprite.fromImage('../static/img/Cartas/BlueCard/0blue.jpg')
-var b1 = Sprite.fromImage('../static/img/Cartas/BlueCard/1blue.jpg')
-var b2 = Sprite.fromImage('../static/img/Cartas/BlueCard/2blue.jpg')
-var b3 = Sprite.fromImage('../static/img/Cartas/BlueCard/3blue.jpg')
-var b4 = Sprite.fromImage('../static/img/Cartas/BlueCard/4blue.jpg')
-var b5 = Sprite.fromImage('../static/img/Cartas/BlueCard/5blue.jpg')
-var b6 = Sprite.fromImage('../static/img/Cartas/BlueCard/6blue.jpg')
-var b7 = Sprite.fromImage('../static/img/Cartas/BlueCard/7blue.jpg')
-var b8 = Sprite.fromImage('../static/img/Cartas/BlueCard/8blue.jpg')
-var b9 = Sprite.fromImage('../static/img/Cartas/BlueCard/9blue.jpg')
-var bs = Sprite.fromImage('../static/img/Cartas/BlueCard/blueBloq.jpg')
-var bra = Sprite.fromImage('../static/img/Cartas/BlueCard/blueCompra.jpg')
-var bre = Sprite.fromImage('../static/img/Cartas/BlueCard/blueInverte.jpg')
-
-//绿色
-var g0 = Sprite.fromImage('../static/img/Cartas/GreenCard/0green.jpg')
-var g1 = Sprite.fromImage('../static/img/Cartas/GreenCard/1green.jpg')
-var g2 = Sprite.fromImage('../static/img/Cartas/GreenCard/2green.jpg')
-var g3 = Sprite.fromImage('../static/img/Cartas/GreenCard/3green.jpg')
-var g4 = Sprite.fromImage('../static/img/Cartas/GreenCard/4green.jpg')
-var g5 = Sprite.fromImage('../static/img/Cartas/GreenCard/5green.jpg')
-var g6 = Sprite.fromImage('../static/img/Cartas/GreenCard/6green.jpg')
-var g7 = Sprite.fromImage('../static/img/Cartas/GreenCard/7green.jpg')
-var g8 = Sprite.fromImage('../static/img/Cartas/GreenCard/8green.jpg')
-var g9 = Sprite.fromImage('../static/img/Cartas/GreenCard/9green.jpg')
-var gs = Sprite.fromImage('../static/img/Cartas/GreenCard/greenBloq.jpg')
-var gra = Sprite.fromImage('../static/img/Cartas/GreenCard/greenCompra.jpg')
-var gre = Sprite.fromImage('../static/img/Cartas/GreenCard/greenInverte.jpg')//红色
-//红色
-var r0 = Sprite.fromImage('../static/img/Cartas/RedCard/0red.png')
-var r1 = Sprite.fromImage('../static/img/Cartas/RedCard/1red.png')
-var r2 = Sprite.fromImage('../static/img/Cartas/RedCard/2red.png')
-var r3 = Sprite.fromImage('../static/img/Cartas/RedCard/3red.png')
-var r4 = Sprite.fromImage('../static/img/Cartas/RedCard/4red.png')
-var r5 = Sprite.fromImage('../static/img/Cartas/RedCard/5red.png')
-var r6 = Sprite.fromImage('../static/img/Cartas/RedCard/6red.png')
-var r7 = Sprite.fromImage('../static/img/Cartas/RedCard/7red.png')
-var r8 = Sprite.fromImage('../static/img/Cartas/RedCard/8red.png')
-var r9 = Sprite.fromImage('../static/img/Cartas/RedCard/9red.png')
-var rs = Sprite.fromImage('../static/img/Cartas/RedCard/redBloq.png')
-var rra = Sprite.fromImage('../static/img/Cartas/RedCard/redCompra.png')
-var rre = Sprite.fromImage('../static/img/Cartas/RedCard/redInverte.png')
-
-//黄色
-var g0 = Sprite.fromImage('../static/img/Cartas/YellowCard/0yellow.jpg')
-var g1 = Sprite.fromImage('../static/img/Cartas/YellowCard/1yellow.jpg')
-var g2 = Sprite.fromImage('../static/img/Cartas/YellowCard/2yellow.jpg')
-var g3 = Sprite.fromImage('../static/img/Cartas/YellowCard/3yellow.jpg')
-var g4 = Sprite.fromImage('../static/img/Cartas/YellowCard/4yellow.jpg')
-var g5 = Sprite.fromImage('../static/img/Cartas/YellowCard/5yellow.jpg')
-var g6 = Sprite.fromImage('../static/img/Cartas/YellowCard/6yellow.jpg')
-var g7 = Sprite.fromImage('../static/img/Cartas/YellowCard/7yellow.jpg')
-var g8 = Sprite.fromImage('../static/img/Cartas/YellowCard/8yellow.jpg')
-var g9 = Sprite.fromImage('../static/img/Cartas/YellowCard/9yellow.jpg')
-var gs = Sprite.fromImage('../static/img/Cartas/YellowCard/yellowBloq.jpg')
-var gra = Sprite.fromImage('../static/img/Cartas/YellowCard/yellowCompra.jpg')
-var gre = Sprite.fromImage('../static/img/Cartas/YellowCard/yellowInverte.jpg')//红色
 //功能牌
-var wild = Sprite.fromImage('../static/img/Cartas/EspecialCard/coringa.png')
-var wildraw = Sprite.fromImage('../static/img/Cartas/EspecialCard/coringaCompra.jpg')*/
+var wild = Sprite.fromImage('../static/img/Cartas/EspecialCard/x.png')
+var wildraw = Sprite.fromImage('../static/img/Cartas/EspecialCard/j.jpg')
 //背面
 var back_card = Sprite.fromImage('../static/img/outras/uno_back.jpg')
+
 //选色按钮(黄，红，绿，蓝) + 操作按钮(出牌，摸牌,+2,+4,uno)
 var yellowbutton = Sprite.fromImage('../static/img/button/yellowbutton.png');
+yellowbutton.interactive = true;
+yellowbutton.buttonMode = true;
+
 var redbutton = Sprite.fromImage('../static/img/button/redbutton.png');
+redbutton.interactive = true;
+redbutton.buttonMode = true;
+
 var greenbutton = Sprite.fromImage('../static/img/button/greenbutton.png');
+greenbutton.interactive = true;
+greenbutton.buttonMode = true;
+
 var bluebutton = Sprite.fromImage('../static/img/button/bluebutton.png');
+bluebutton.interactive = true;
+bluebutton.buttonMode = true;
+
 var color_container = new container();
+color_container.addChild(yellowbutton);
+color_container.addChild(redbutton);
+color_container.addChild(greenbutton);
+color_container.addChild(bluebutton);
 
 var outcard = Sprite.fromImage('../static/img/button/outcard.png');
+outcard.interactive = true;
+outcard.buttonMode = true;
+
 var getcard = Sprite.fromImage('../static/img/button/getcard.png');
+getcard.interactive = true;
+getcard.buttonMode = true;
+
 var uno = Sprite.fromImage('../static/img/button/uno.png');
+uno.interactive = true;
+uno.buttonMode = true;
+
 var gettwo = Sprite.fromImage('../static/img/button/addtwo.png');
+gettwo.interactive = true;
+gettwo.buttonMode = true;
+
 var getfour = Sprite.fromImage('../static/img/button/addfour.png');
+getfour.interactive = true;
+getfour.buttonMode = true;
 
 //四副牌
 var dong_container = new container();
@@ -90,7 +62,7 @@ var xi_container = new container();
 var bei_container = new container();
 
 //东南西北标记(东南西北 + 玩家名称)
-var dong = new PIXI.Text('aa');
+var dong = new PIXI.Text('东');
 var nan = new PIXI.Text('南');
 var xi = new PIXI.Text('西');
 var bei = new PIXI.Text('北');
@@ -102,6 +74,10 @@ var center_cards = new container();
 
 //剩余牌数(东南西北)
 var dong_number,nan_number,xi_number,bei_number;
+var remaining_dong = new PIXI.Text('剩余牌数');
+var remaining_xi = new PIXI.Text('剩余牌数');
+var remaining_nan = new PIXI.Text('剩余牌数');
+var remaining_bei = new PIXI.Text('剩余牌数');
 
 //顺时针逆时针方向
 var direction = 0;
@@ -109,7 +85,6 @@ var ssz = new Sprite.fromImage("../static/img/shunshizhen.png")
 var nsz = new Sprite.fromImage("../static/img/nishizhen.png")
 
 //功能牌效果展示(选色(红,黄,蓝,绿)，+2，+4，反向，停止，uno)
-var select_color;
 var select_green = new PIXI.Text('绿色');
 var select_red = new PIXI.Text('红色');
 var select_blue = new PIXI.Text('蓝色');
@@ -128,34 +103,129 @@ app.stage.addChild(dong_container);
 app.stage.addChild(xi_container);
 app.stage.addChild(nan_container);
 app.stage.addChild(bei_container);
-//玩家这个位置牌堆x400,y450,牌的大小0.4,y相等,x为18相距
+
+app.stage.addChild(center_cards);
+//玩家这个位置牌堆x500,y530,牌的大小0.4,y相等,x为18相距
+//其他人的牌比例均是0.3,左手边x150，y200,x相等,y间距18,rotation1.57
+//对面x800,y100,x相距18，y相等,比例0.3,rotation3.14
+//右手边x1050,y300,rotation4.71，比例0.3,x相等，y相距18
+//随机方向(Math.random()*(6-0) + 0).toFixed(3)
+//东南西北标记 玩家x400,y500 左手边rotation1.57,x150,y100 对面rotation3.14,x900,y100
+//右手边x1050 y300,rotation4.71
+
+//按钮的位置400,400,选择x相距120
+//效果展示位置在x600,y350
 var basepath = '../static/img/Cartas/';
-var bluepath = 'BlueCard/'
+var bluepath = 'BlueCard/';
+var redpath = 'RedCard/';
+var greenpath = 'GreenCard/';
+var yellowpath = 'YellowCard/';
+var specialpath = 'EspecialCard/';
 for(var i = 0;i < 3;i++)
 {
-    var one = new Sprite.fromImage('../static/img/Cartas/BlueCard/'+'0blue.jpg')
+    var one = new Sprite.fromImage(basepath + bluepath + '0.jpg')
     one.x = i*18;
+    one.anchor.set(0.5);
     one.y = 0;
     one.scale.x = 0.4;
     one.scale.y = 0.4;
+    one.interactive = true;
+    one.buttonMode = true;
     dong_container.addChild(one);
 }
-dong_container.x = 400;
-dong_container.y = 450;
+dong_container.x = 500;
+dong_container.y = 530;
+dong.x = 400
+dong.y = 500;
+app.stage.addChild(dong);
 
-//其他人的牌比例均是0.3
 for(var i = 0;i < 3;i++)
 {
-    var one = back_card
-    one.x = i*18;
-    one.y = 0;
+    var one = new Sprite.fromImage('../static/img/outras/uno_back.jpg')
+    one.x = 0;
+    one.anchor.set(0.5);
+    one.y = i*18;
     one.scale.x = 0.3;
     one.scale.y = 0.3;
     one.rotation =1.57; //1.57
     xi_container.addChild(one);
 }
-xi_container.x = 400;
-xi_container.y = 450;
+xi_container.x = 150;
+xi_container.y = 200;
+xi.x = 150;
+xi.y = 100;
+xi.rotation = 1.57;
+app.stage.addChild(xi);
+
+for(var i = 0;i < 3;i++)
+{
+    var one = new Sprite.fromImage('../static/img/outras/uno_back.jpg')
+    one.x = i*18;
+    one.y = 0;
+    one.anchor.set(0.5);
+    one.scale.x = 0.3;
+    one.scale.y = 0.3;
+    one.rotation =3.14; //1.57
+    nan_container.addChild(one);
+}
+nan_container.x = 800;
+nan_container.y = 100;
+nan.x = 900;
+nan.y = 100;
+nan.rotation = 3.14;
+app.stage.addChild(nan);
 
 
+for(var i = 0;i < 3;i++)
+{
+    var one = new Sprite.fromImage('../static/img/outras/uno_back.jpg')
+    one.x = 0;
+    one.y = i*18;
+    one.anchor.set(0.5);
+    one.scale.x = 0.3;
+    one.scale.y = 0.3;
+    one.rotation =4.71; //1.57
+    bei_container.addChild(one);
+}
+bei_container.x = 1050;
+bei_container.y = 300;
+bei.x = 1050;
+bei.y = 400;
+bei.rotation = 4.71;
+app.stage.addChild(bei);
+
+
+for(var i = 0;i < 3;i++)
+{
+    var one = new Sprite.fromImage(basepath + bluepath + '0.jpg')
+    one.x = 0;
+    one.y = 0;
+    one.anchor.set(0.5);
+    one.scale.x = 0.4;
+    one.scale.y = 0.4;
+    one.rotation = (Math.random()*(6-0) + 0).toFixed(3);
+    center_cards.addChild(one);
+}
+center_cards.x = 650;
+center_cards.y = 250;
+
+app.stage.addChild(color_container);
+color_container.x = 400;
+color_container.y = 400;
+redbutton.x = 0,redbutton.y = 0;
+greenbutton.x = 120,redbutton.y = 0;
+yellowbutton.x  = 240,yellowbutton = 0;
+bluebutton.x = 360,bluebutton.y = 0;
+
+
+app.stage.addChild(select_red);
+select_red.x = 600;
+select_red.y = 350;
+
+app.stage.addChild(ssz);
+ssz.anchor.set(0.5);
+ssz.x = 650;
+ssz.y = 250;
+ssz.scale.x = 2.3;
+ssz.scale.y = 2.3;
 
