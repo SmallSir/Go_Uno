0b073df63cac8617a04f373a10cba381dda8ae86
WhaleFall <280690956@qq.com>
2019-04-21 23:12:27 +0800

    修改路由

--- Uno/routers/router.go
+++ Uno/routers/router.go
@@ -8,11 +8,11 @@ import (
 
 func init() {
 	//首页
-	beego.Router("/", &controllers.UserController{})
+	beego.Router("/", &controllers.UserController{}, "get:Login")
 	//登录
 	beego.Router("/login", &controllers.UserController{}, "post:Login")
 	//注册页面
-	beego.Router("/register", &controllers.UserController{}, "get:GetRegister")
+	beego.Router("/register", &controllers.UserController{}, "get:Register")
 	//注册
 	beego.Router("/register", &controllers.UserController{}, "post:Register")
 	//发送邮箱验证码
@@ -22,7 +22,7 @@ func init() {
 	//创建房间
 	beego.Router("/create", &controllers.GameController{}, "post:Register")
 	//进入房间
-	beego.Router("/join", &controllers.GameController, "post:Join")
+	beego.Router("/join", &controllers.GameController{}, "post:Join")
 	//房间
 	beego.Router("/uno/play?userid=:id?roomid=:roomname", &controllers.GameController{}, "get:ConnectionWebSocket")
 
