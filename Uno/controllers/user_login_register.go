ced88802846143f336fe6b704b51f9ca8cdfe73e
WhaleFall <280690956@qq.com>
2019-04-22 14:38:51 +0800

    重新实现了自动发送验证码的功能

--- Uno/controllers/user_login_register.go
+++ Uno/controllers/user_login_register.go
@@ -1,10 +1,15 @@
 package controllers
 
 import (
+	"crypto/tls"
 	"encoding/json"
+	"log"
+	"math/rand"
+	"time"
+
+	"github.com/go-gomail/gomail"
 
 	"github.com/astaxie/beego"
-	"github.com/astaxie/beego/utils"
 )
 
 //内容为测试
@@ -103,6 +108,7 @@ func (User *UserController) Login() {
 
 //Register 是实现用户注册功能
 func (User *UserController) Register() {
+	User.TplName = "register.html"
 	//返回信息
 	ok := false
 	msg := ""
@@ -155,23 +161,28 @@ func (User *UserController) Register() {
 
 //向邮箱传递验证码
 func (User *UserController) EmailCheck() {
+	User.TplName = "register.html"
 	//获取邮箱信息
 	user := Userlogin{}
 	err := json.Unmarshal([]byte(User.Ctx.Input.RequestBody), user)
 	email := user.Email
-
+	log.Println([]byte(User.Ctx.Input.RequestBody))
+	log.Println(user.Email)
 	//返回信息
 	ok := false
 	msg := ""
-	//url := ""
+	if err != nil {
+		msg = err.Error()
+		return
+	}
 	defer func() {
 		remsg := &ReRoomMsg{}
 		remsg.Msg = msg
 		remsg.Ok = ok
-		//remsg.Url = url
 		ret, _ := json.Marshal(remsg)
 		User.Data["json"] = string(ret)
 		User.EnableRender = false
+		log.Println("这还是测试")
 		User.ServeJSON()
 	}()
 
@@ -180,31 +191,20 @@ func (User *UserController) EmailCheck() {
 	*/
 
 	//生成一个六位验证码
-	//yzm := rand.New(rand.NewSource(time.Now().UnixNano())).Int31n(1000000)
+	yzm := rand.New(rand.NewSource(time.Now().UnixNano())).Int31n(1000000)
 
-	// 创建一个字符串变量，存放邮件的配置信息
-	config :=
-		`{"username":"280690956@qq.com","password":"ylrwvidjqlagbhjb","host":"smtp.qq.com","port":25}`
-	// 通过存放配置信息的字符串，创建Email对象
-	temail := utils.NewEMail(config)
-	// 指定邮件的基本信息
-	temail.To = []string{email}      //指定收件人邮箱地址
-	temail.From = "280690956@qq.com" //指定发件人的邮箱地址
-	temail.Subject = "UNO账号注册验证码"    //指定邮件的标题
-	temail.HTML = `<html>
-        <head>
-        </head>
-            <body>
-            <div>UNO的验证码为 12345 </div>
-            </body>
-        </html>` //指定邮件内容
-	// 发送邮件
-	err = temail.Send()
-	if err != nil {
-		msg = "邮件发送失败，请重新确认邮箱或更改邮箱"
+	sendemail := "<h1>验证码" + string(yzm) + "</h1>"
+	m := gomail.NewMessage()
+	m.SetAddressHeader("From", "280690956@qq.com", "UNO官方")
+	m.SetAddressHeader("To", email, email)
+	m.SetHeader("Subject", "UNO账号注册验证码")
+	m.SetBody("text/html", sendemail)
+	d := gomail.NewPlainDialer("smtp.qq.com", 25, "280690956@qq.com", "ylrwvidjqlagbhjb")
+	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+	if err := d.DialAndSend(m); err != nil {
+		msg = err.Error()
 		return
 	}
-
 	/*
 		把验证码和邮箱同时发送到redis数据库保留，并限时2分钟
 	*/
