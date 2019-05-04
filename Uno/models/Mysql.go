8385dfe172bddacb9a7c7e5e21914a59aad5d851
WhaleFall <280690956@qq.com>
2019-05-04 21:12:11 +0800

    修复了MySQL无法启动的bug

--- Uno/models/Mysql.go
+++ Uno/models/Mysql.go
@@ -1,12 +1,15 @@
 package models
 
 import (
+	"log"
+
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 //初始化数据库的状态
-func Init() {
+func init() {
 	dbhost := beego.AppConfig.String("dbhost")         //MySQL数据库的地址
 	dbport := beego.AppConfig.String("dbport")         //MySQL数据库的端口号
 	dbuser := beego.AppConfig.String("dbuser")         //MySQL数据库的用户名
@@ -15,9 +18,17 @@ func Init() {
 	if dbport == "" {
 		dbport = "3306"
 	}
-	dsn := dbuser + ":" + dbpassword + "@tcp(" + dbhost + ":" + dbport + ")/" + dbname + "?charset=utf8&loc=Asia%2FShanghai"
-	orm.RegisterDataBase("default", "mysql", dsn) //注册一个名为default的数据库
-	orm.RegisterModel(new(User))                  //对定义的modle创建
+	dsn := dbuser + ":" + dbpassword + "@tcp(" + dbhost + ":" + dbport + ")/" + dbname + "?charset=utf8"
+	if err := orm.RegisterDriver("mysql", orm.DRMySQL); err != nil {
+		log.Println("注册driver有错")
+	}
+	if err := orm.RegisterDataBase("default", "mysql", dsn); err != nil {
+		log.Println("数据库注册有误")
+	} //注册一个名为default的数据库
+	orm.RegisterModel(new(User)) //对定义的modle创建
+	if err := orm.RunSyncdb("default", false, true); err != nil {
+		log.Println("数据库运行失败")
+	}
 }
 
 type User struct {
@@ -26,9 +37,4 @@ type User struct {
 	Name     string
 	Password string
 	Score    int
-}
-
-//返回带前缀的表名
-func TableName(str string) string {
-	return beego.AppConfig.String("dbprefix") + str
 }
